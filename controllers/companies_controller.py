f05824e160b846489f4670ffaf0c06e0749300ee
AvianLearning <iaradcliffe@hotmail.com>
2020-10-03 19:53:36 +0100

    Creating companies controller

--- controllers/companies_controller.py
+++ controllers/companies_controller.py
@@ -11,3 +11,31 @@ def companies():
     companies = company_repository.select_all()
     return render_template("companies/index.html", companies=companies)
 
+# Add company
+@companies_blueprint.route("/companies/add")
+def add_company():
+    return render_template("companies/add.html")
+
+# Create and return to companies page ??
+@companies_blueprint.route("/companies", methods=["POST"])
+def create_company():
+    name = request.form["name"]
+    new_company = Company(name)
+    company_repository.save(new_company)
+    return redirect("/companies")
+
+# Edit company
+@companies_blueprint.route("/companies/<id>/edit")
+def edit_company(id):
+    company = company_repository.select(id)
+    return render_template("companies/edit.html", company=company)
+
+
+# Update company and return to companies landing page
+@companies_blueprint.route("/companies/<id>", methods=["POST"])
+def update_company(id):
+    name = request.form["name"]
+    company = Company(name, id)
+    company_repository.update(company)
+    return redirect("/companies")
+
