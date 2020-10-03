b36aafa645852928d8c791a61e1965b016722d9b
AvianLearning <iaradcliffe@hotmail.com>
2020-10-03 12:06:27 +0100

    Created repos for company, tag, transaction

---
+++ models/repositories/company_repository.py
@@ -0,0 +1,47 @@
+from db.run_sql import run_sql
+
+from models.company import Company
+
+def save(company):
+    sql = "INSERT INTO companies (name) VALUES (%s) RETURNING id"
+    values = [company.name]
+    results = run_sql(sql, values)
+    company.id = results[0]['id']
+    return company
+
+
+def select_all():
+    companies = []
+
+    sql = "SELECT * FROM companies"
+    results = run_sql(sql)
+
+    for row in results:
+        company = Company(row['name'], row['id'])
+        companies.append(company)
+    return companies
+
+
+def select(id):
+    company = None
+    sql = "SELECT * FROM companies WHERE id=%s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        company = Company(result['name'], result['id'])
+    return company
+
+
+def delete_all():
+    sql = "DELETE from companies"
+    run_sql(sql)
+
+
+def delete(id):
+    sql = "DELETE FROM companies WHERE id=%s"
+    values = [id]
+    run_sql(sql, values)
+
+
+def update()
