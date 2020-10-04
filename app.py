07bba8b61038cba982c5741e29b93959b960f401
AvianLearning <iaradcliffe@hotmail.com>
2020-10-04 09:18:53 +0100

    Added blueprints to app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
 
-
+from controllers.transactions_controller import transactions_blueprint
+from controllers.companies_controller import companies_blueprint
+from controllers.tags_controller import tags_blueprint
 
 app = Flask(__name__)
 
 # register all the blueprints - company, tag, transaction
+app.register_blueprint(transactions_blueprint)
+app.register_blueprint(companies_blueprint)
+app.register_blueprint(tags_blueprint)
 
 @app.route("/")
 def main():
