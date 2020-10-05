fd2002a5aed23471ed570700b1f44468b5af6f7e
AvianLearning <iaradcliffe@hotmail.com>
2020-10-05 10:07:07 +0100

    Styling header, setting up grid

--- console.py
+++ console.py
@@ -22,6 +22,15 @@ company_repository.save(company_2)
 company_3 = Company("WideNation")
 company_repository.save(company_3)
 
+company_4 = Company("Blub")
+company_repository.save(company_4)
+
+company_5 = Company("Amoosin Primula")
+company_repository.save(company_5)
+
+company_6 = Company("Blumen Ecke")
+company_repository.save(company_6)
+
 tag_1 = Tag("travel")
 tag_repository.save(tag_1)
 
@@ -30,6 +39,15 @@ tag_repository.save(tag_2)
 
 tag_3 = Tag("mortgage")
 tag_repository.save(tag_3)
+
+tag_4 = Tag("utilities")
+tag_repository.save(tag_4)
+
+tag_5 = Tag("entertainment")
+tag_repository.save(tag_5)
+
+tag_6 = Tag("floristry")
+tag_repository.save(tag_6)
 
 transaction_1 = Transaction(150.00, company_1, tag_1)
 transaction_repository.save(transaction_1)
@@ -40,13 +58,17 @@ transaction_repository.save(transaction_2)
 transaction_3 = Transaction(750.00, company_3, tag_3)
 transaction_repository.save(transaction_3)
 
+transaction_4 = Transaction(70.34, company_4, tag_4)
+transaction_repository.save(transaction_4)
+
+transaction_5 = Transaction(8.50, company_5, tag_5)
+transaction_repository.save(transaction_5)
+
+transaction_6 = Transaction(28.99, company_6, tag_6)
+transaction_repository.save(transaction_6)
+
 transactions = transaction_repository.select_all()
 
-company_1 = Company("LNER")
-company_repository.update(company_1)
-
-tag_2 = Tag("drink")
-tag_repository.update(tag_2)
 
 pdb.set_trace()
 
